dce6312c86c0178544430c07ace24464f02601d3
nicolasribeiroo <git@niskii.dev>
2026-01-30 13:20:54 -0300

    chore: update deps

--- mix.exs
+++ mix.exs
@@ -1,12 +1,21 @@
-defmodule Abacatepay.MixProject do
+defmodule AbacatePay.MixProject do
   use Mix.Project
+
+  @app :abacatepay
+  @description "AbacatePay Elixir SDK for you to start receiving payments in seconds"
+  @name "AbacatePay"
+  @version "0.1.0"
+  @source_url "https://github.com/AbacatePay/abacatepay-elixir-sdk"
 
   def project do
     [
-      app: :abacatepay,
-      version: "0.0.0",
+      app: @app,
+      version: @version,
       elixir: "~> 1.14",
+      description: @description,
       start_permanent: Mix.env() == :prod,
+      name: @name,
+      source_url: @source_url,
       deps: deps()
     ]
   end
@@ -20,8 +29,8 @@ defmodule Abacatepay.MixProject do
   defp deps do
     [
       {:jason, "~> 1.4"},
-      {:httpoison, "~> 2.0"},
-      {:ex_doc, "~> 0.29", only: :dev, runtime: false}
+      {:httpoison, "~> 2.3"},
+      {:ex_doc, "~> 0.40.0", only: :dev, runtime: false}
     ]
   end
-end 
+end
